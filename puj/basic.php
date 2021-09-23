f7368b934945fce4aa45e14ba2476c317676d826
Aman2907 <74008888+Aman2907@users.noreply.github.com>
2021-09-23 20:38:08 +0530

    Add files via upload

---
+++ puj/basic.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP</title>
+</head>
+<body>
+    <div class="container">
+        great world
+    <?php
+
+    echo " Hello This is my First php website\n";
+
+
+    $variable1=34;
+    $variable2=45;
+    echo $variable1;
+    echo $variable2;
+
+    echo "<br>";
+    echo $variable1 + $variable2;
+
+    echo "<br>";
+    $lab =array("AMAN", "PUJA", "Srikant", "Sooraj");
+    echo count($lab); 
+    echo $lab[1]; //array collection of values
+       
+
+     ?>   
+     
+     
+
+
+    </div>
+</body>
+</html>
